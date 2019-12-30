3ec79a2585580013cb89541c89e04e788711df6b
Dmytro A Frolov <dmytro.a.frolov@globallogic.com>
2019-12-30 12:01:15 +0200

    appium example

--- src/test/java/com/github/frunoman/tests/BaseTest.java
+++ src/test/java/com/github/frunoman/tests/BaseTest.java
@@ -1,30 +1,45 @@
 package com.github.frunoman.tests;
 
+import com.android.prefs.AndroidLocation;
+import com.android.repository.api.ProgressIndicator;
+import com.android.repository.api.RepoManager;
+import com.android.sdklib.IAndroidTarget;
+import com.android.sdklib.ISystemImage;
+import com.android.sdklib.devices.DeviceManager;
+import com.android.sdklib.internal.avd.AvdInfo;
+import com.android.sdklib.internal.avd.AvdManager;
+import com.android.sdklib.repository.AndroidSdkHandler;
+import com.android.sdklib.repository.targets.AndroidTargetManager;
+import com.android.sdklib.repository.targets.SystemImageManager;
+import com.android.sdklib.tool.SdkManagerCli;
+import com.android.utils.ILogger;
+import com.android.utils.SdkUtils;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.github.frunoman.pages.MainPage;
 import com.github.frunoman.utils.Utils;
+import com.google.common.io.Resources;
 import io.appium.java_client.MobileElement;
 import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.android.SupportsSpecialEmulatorCommands;
 import io.appium.java_client.remote.AndroidMobileCapabilityType;
 import io.appium.java_client.remote.MobileCapabilityType;
 import io.appium.java_client.service.local.AppiumDriverLocalService;
 import io.appium.java_client.service.local.AppiumServiceBuilder;
 import io.appium.java_client.service.local.flags.GeneralServerFlag;
+import io.appium.java_client.service.local.flags.ServerArgument;
 import org.openqa.grid.internal.utils.configuration.GridHubConfiguration;
 import org.openqa.grid.internal.utils.configuration.GridNodeConfiguration;
 import org.openqa.grid.web.Hub;
 import org.openqa.selenium.MutableCapabilities;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.testng.annotations.*;
+import org.testng.annotations.Optional;
 
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.net.*;
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.List;
-import java.util.Properties;
+import java.util.*;
 import java.util.concurrent.TimeUnit;
 
 public class BaseTest {
@@ -41,14 +56,13 @@ public class BaseTest {
     private static final String APP_WAIT_ACTIVITY = "com.github.allureadvanced.*";
     private static final String ALLURE_RESULTS_ZIP = "/sdcard/allure-results.zip";
     protected static final String ALLURE_RESULT_FILE = "allure-results.zip";
+    protected AvdManager avdManager;
 
-    private ClassLoader classLoader;
 
     @BeforeSuite
     public void beforeSuite() throws IOException {
-        classLoader = getClass().getClassLoader();
         properties = new Properties();
-        properties.load(new FileInputStream(new File(classLoader.getResource(PROJECT_PROPERTIES).getPath())));
+        properties.load(new FileInputStream(new File(Resources.getResource(PROJECT_PROPERTIES).getPath())));
 
         if (gridServer == null) {
             localhost = InetAddress.getLocalHost().getHostAddress();
@@ -63,17 +77,22 @@ public class BaseTest {
         }
     }
 
-    @Parameters({"udid"})
+    @Parameters({"udid","version"})
     @BeforeTest
-    public void beforeTest(@Optional String udid) throws IOException, InterruptedException {
+    public void beforeTest(@Optional String udid,@Optional String version) throws IOException, InterruptedException {
         GridNodeConfiguration nodeConfiguration = new GridNodeConfiguration();
         nodeConfiguration.hubHost = localhost;
         nodeConfiguration.hubPort = port;
 
-        DesiredCapabilities capabilities = new DesiredCapabilities();
-        capabilities.setCapability(MobileCapabilityType.BROWSER_NAME, "Android");
+        DesiredCapabilities capabilities = DesiredCapabilities.android();
         capabilities.setCapability(MobileCapabilityType.PLATFORM_NAME, "Android");
+        capabilities.setCapability(MobileCapabilityType.PLATFORM, "Android");
 
+        if (version!=null){
+            capabilities.setCapability(MobileCapabilityType.PLATFORM_VERSION,version);
+            capabilities.setCapability("version",version);
+
+        }
         List<MutableCapabilities> capabilitiesList = new ArrayList<MutableCapabilities>();
         capabilitiesList.add(capabilities);
         nodeConfiguration.capabilities = capabilitiesList;
@@ -92,21 +111,28 @@ public class BaseTest {
         service = AppiumDriverLocalService.buildService(builder);
         service.start();
 
-        Thread.sleep(8000);
-
+        Thread.sleep(10000);
         DesiredCapabilities appiumCapabilities = new DesiredCapabilities();
+        if (udid!=null){
+            appiumCapabilities.setCapability(MobileCapabilityType.UDID,udid);
+        }
+        if (version!=null){
+            appiumCapabilities.setCapability(MobileCapabilityType.PLATFORM_VERSION,version);
+        }
         appiumCapabilities.setCapability(MobileCapabilityType.PLATFORM_NAME, "Android");
         appiumCapabilities.setCapability(MobileCapabilityType.DEVICE_NAME, "device");
-        appiumCapabilities.setCapability(MobileCapabilityType.DEVICE_NAME, "UiAutomator2");
-        appiumCapabilities.setCapability(MobileCapabilityType.APP, classLoader.getResource("app-debug.apk").getPath());
+        appiumCapabilities.setCapability(MobileCapabilityType.AUTOMATION_NAME, "UiAutomator2");
+
+        appiumCapabilities.setCapability(MobileCapabilityType.APP, Resources.getResource("app-debug.apk").getPath());
         appiumCapabilities.setCapability(AndroidMobileCapabilityType.APP_WAIT_ACTIVITY, APP_WAIT_ACTIVITY);
+        appiumCapabilities.setCapability(AndroidMobileCapabilityType.AUTO_GRANT_PERMISSIONS, true);
         appiumCapabilities.setCapability(AndroidMobileCapabilityType.SYSTEM_PORT, Utils.nextFreePort(5672, 5690));
 
         driver = new AndroidDriver<MobileElement>(new URL(gridServer.getUrl().toString() + "/wd/hub"), appiumCapabilities);
         driver.allowInvisibleElements(true);
         driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
 
-        driver.pushFile(ALLURE_RESULTS_ZIP, new File(classLoader.getResource(ALLURE_RESULT_FILE).getPath()));
+        driver.pushFile(ALLURE_RESULTS_ZIP, new File(Resources.getResource(ALLURE_RESULT_FILE).getPath()));
 
     }
 
